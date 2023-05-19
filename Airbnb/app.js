0ffe241e8ac7f3723eef8217091d7c37f9a93716
nitesh <nitesh8825kr@gmail.com>
2023-05-19 09:42:11 +0530

    i have edited my js file add dark mod and save into local storage and add hamburger functionality

--- Airbnb/app.js
+++ Airbnb/app.js
@@ -1,1 +1,34 @@
-console.log("hello");
+// ========================== color change effect  =============================
+const color_change = document.querySelector(".color_change");
+
+color_change.addEventListener("click", () => {
+  document.documentElement.classList.toggle("Dark_Mod");
+  if (localStorage.getItem("theme") === "light_mod") {
+    localStorage.setItem("theme", "dark_mod");
+  } else {
+    localStorage.setItem("theme", "light_mod");
+  }
+});
+
+// ========================== store color change effect in local_storage  =============================
+
+if (localStorage.getItem("theme") === "light_mod") {
+  document.documentElement.classList.remove("Dark_Mod");
+}
+// =======================================
+else if (localStorage.getItem("theme") === "dark_mod") {
+  document.documentElement.classList.add("Dark_Mod");
+}
+// =======================================
+else {
+  localStorage.setItem("theme", "light_mod");
+}
+
+// ========================== navbar mobile effect  =============================
+
+const show = document.querySelector(".show");
+const nav_links = document.querySelector(".nav_links");
+
+show.addEventListener("click", () => {
+  nav_links.classList.toggle("show");
+});
